20625981b5f99ccf2819afd481f71ca1620b4690
Kenan20222 <104547432+Kenan20222@users.noreply.github.com>
2022-05-27 18:45:15 +0400

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "1234567890:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	token: "5448417859:AAEtAFN3rsc946JY1Y3CWHiAGnPz-hpH34U",
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
 	timerSteps: 3, //timer indicators
